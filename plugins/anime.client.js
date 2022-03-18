c87c89ae3c43fd29c164f570fd82faf2287a3e50
ArthurDev <arthur@imobia.app>
2022-03-18 08:30:38 -0300

    Changes anime directive

--- plugins/anime.client.js
+++ plugins/anime.client.js
@@ -5,15 +5,27 @@ import anime from 'animejs/lib/anime.es.js'
 export default defineNuxtPlugin((nuxtApp) => {
   let animeDir = {
     created(el, binding) {
-      if (binding.arg) {
-        let animation = anime({
-          ...{
-            targets: el,
-            autoplay: false,
-          },
-          ...binding.value.animation,
+      let targets
+      console.log(binding.value.target)
+      if (typeof binding.value.target === 'string') {
+        targets = el[binding.value.target]
+      } else {
+        targets = el
+      }
+      let animation = anime({
+        ...{
+          targets,
+          autoplay: false,
+        },
+        ...binding.value,
+      })
+
+      if (!binding.arg) {
+        animation.play()
+      } else {
+        el.addEventListener(binding.arg, () => {
+          animation.play()
         })
-        binding.created(animation, value.animation)
       }
     },
   }
