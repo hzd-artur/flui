a4bf7b12e11ba5a1e769493123a77900ffbc402a
ArthurDev <arthur@imobia.app>
2022-03-17 23:51:10 -0300

    template

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,10 @@
 <template>
   <div class="app">
-    <a-header></a-header>
-
     <div
       style="overflow: overlay;"
-      class="fl-grow-1 fl-column d-flex"
-      :class="
-        darkMode
-          ? 'background-purple-shade-9 text-purple-tint-9'
-          : 'background-purple-tint-9 text-purple-shade-9'
-      "
+      class="fl-grow-1 fl-column d-flex background-purple-tint-9 text-purple-shade-9"
     >
       <slot />
-
-      <a-footer></a-footer>
     </div>
   </div>
 </template>
-<script setup>
-const darkMode = useState('darkMode', () => false)
-</script>
-<script>
-export default {
-  data: () => ({
-    scroll: {},
-  }),
-  methods: {
-    setTheme() {
-      this.darkMode = !this.darkMode
-      localStorage.setItem('theme', this.darkMode ? 'dark' : 'light')
-    },
-  },
-  mounted() {
-    /*    let result = sass.compileString(`@import './colors.scss';`)
-    console.log(result) */
-    let isDark = false
-
-    const theme = localStorage.getItem('theme')
-    if (theme) {
-      if (theme === 'dark') {
-        this.darkMode = true
-      } else {
-        this.darkMode = false
-      }
-    } else if (
-      window.matchMedia &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches
-    ) {
-      this.darkMode = true
-      localStorage.setItem('theme', 'dark')
-    }
-  },
-}
-</script>
